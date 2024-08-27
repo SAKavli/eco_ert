b86e8af70b43125076a1ac4a9bdcbf6a37c5f06a
Sander Kavli <skav@equinor.com>
2024-08-27 14:32:02 +0200

    Upload all current files from current project stage to repository

---
+++ scripts/button.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+def get_user_input(info_text):
+    #Setup of window
+    root = tk.Tk()
+    root.geometry("800x250")
+    root.resizable(False,False)
+    root.title("Input to eco.ert")
+
+    #Setup of input variables
+    housePrice = tk.StringVar()
+    equity = tk.StringVar()
+    salary = tk.StringVar()
+
+    def ok_clicked():
+        root.destroy()
+
+    #Input frame
+    dialoguebox = ttk.Frame(root)
+    dialoguebox.pack(padx=10, pady=10, fill='x', expand=True)
+    
+    #Info label
+    info_label = ttk.Label(dialoguebox, text = info_text)
+    info_label.pack(fill="x",expand=True)
+
+    #House price
+    house_label = ttk.Label(dialoguebox, text = "What is the price of the house?")
+    house_label.pack(fill="x",expand=True)
+
+    house_entry = ttk.Entry(dialoguebox, textvariable=housePrice)
+    house_entry.pack(fill="x", expand=True)
+    house_entry.focus()
+
+    #Equity
+    equity_label = ttk.Label(dialoguebox, text = "What is your equity in this purchase?")
+    equity_label.pack(fill="x",expand=True)
+
+    equity_entry = ttk.Entry(dialoguebox, textvariable=equity)
+    equity_entry.pack(fill="x", expand=True)
+    equity_entry.focus()
+
+    #Salary
+    salary_label = ttk.Label(dialoguebox, text = "What is your yearly salary? (Brutto)")
+    salary_label.pack(fill="x",expand=True)
+
+    salary_entry = ttk.Entry(dialoguebox, textvariable=salary)
+    salary_entry.pack(fill="x", expand=True)
+    salary_entry.focus()
+
+    #OK button
+    ok_button = ttk.Button(dialoguebox, text="Simulate my loan", command=ok_clicked)
+    ok_button.pack(fill="x", expand=True, pady=10)
+
+    root.mainloop()
+    
+    if housePrice.get() == "" and equity.get() == "" and salary.get() == "":
+        exit_workflow()
+
+    try:
+        int(housePrice.get())
+        int(equity.get())
+        int(salary.get())
+    except ValueError:
+        return False
+
+    with open("userinput.txt", "w", encoding="utf-8") as f:
+        f.write(f"{housePrice.get()} {equity.get()} {salary.get()}")
+    return True
+
+def exit_workflow():
+    #Setup of window
+    root = tk.Tk()
+    root.geometry("800x250")
+    root.resizable(False,False)
+    root.title("Exit workflow setup")
+    #Input frame
+    dialoguebox = ttk.Frame(root)
+    dialoguebox.pack(padx=10, pady=10, fill='x', expand=True)
+
+    #Exit info
+    equity_label = ttk.Label(dialoguebox, text = "Workflow exited, setup incomplete")
+    equity_label.pack(fill="x",expand=True)
+    root.mainloop()
+    exit()
+
+input_is_valid = get_user_input("Hi! Please fill in your information to simulate when you will be debt free!\n\n")
+
+while not input_is_valid:  
+    input_is_valid = get_user_input("Sorry, you have entered something invalid, please enter only numbers")
---
+++ scripts/eco_eval.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import json
+from time import sleep as sleep
+
+def evaluate(coeffs,debt,salary):
+    monthly_expenses = 5000
+    int_rate = coeffs["interest_rate"]
+    tax = coeffs["tax"]
+
+    monthly_salary = salary/12 * (1-tax/100)
+    monthly_interest = debt*int_rate/1200
+    monthly_repay = monthly_salary - monthly_interest - monthly_expenses
+    return max(0,(debt - monthly_repay))
+
+def main():
+    #Read coeff values
+    with open("parameters.json", encoding="utf-8") as f:
+        coeffs = json.load(f)["COEFFS"]
+    
+
+    #Read user input
+    with open("userinput.txt", "r", encoding="utf-8") as f:
+        house_price, equity, salary = [int(x) for x in f.readline().split()]
+    #Initialize debt value
+    debt =  house_price - equity
+    i = 0
+    output = [debt]
+    while debt>0:
+        debt = evaluate(coeffs,debt,salary)
+        output.append(debt)
+        i += 1
+        """
+        When there has been 100 realizations and
+        the monthly repay is smaller than the
+        interest, terminate the simulation
+        """
+        if i == 100 and output[-2] < output[-1]:
+            break
+
+    with open("eco.out", "w", encoding="utf-8") as f:
+        f.write("\n".join(map(str,output)))
+
+if __name__ == "__main__":
+    main()
