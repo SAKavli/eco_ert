99ab71f0857ccd11394c008e3961364607e43d17
eq-sandkav <skav@equinor.com>
2024-08-29 09:26:42 +0200

    Add documentation and better user instructions to button.py

--- scripts/button.py
+++ scripts/button.py
@@ -6,7 +6,7 @@ from tkinter.messagebox import showinfo
 def get_user_input(info_text):
     #Setup of window
     root = tk.Tk()
-    root.geometry("800x250")
+    root.geometry("800x350")
     root.resizable(False,False)
     root.title("Input to eco.ert")
 
@@ -16,6 +16,7 @@ def get_user_input(info_text):
     salary = tk.StringVar()
     monthly_expenses = tk.StringVar()
 
+    #Close window when submit button is pressed
     def ok_clicked():
         root.destroy()
 
@@ -51,7 +52,7 @@ def get_user_input(info_text):
     salary_entry.pack(fill="x", expand=True)
     salary_entry.focus()
     
-    #Salary
+    #Montly expenses
     monthly_expenses_label = ttk.Label(dialoguebox, text = "What is your estimated monthly expenses from groceries, spare time activities/equipment etc.?")
     monthly_expenses_label.pack(fill="x",expand=True)
 
@@ -65,21 +66,25 @@ def get_user_input(info_text):
 
     root.mainloop()
     
+    #Either if user submits empty imput boxes or exits the window while input boxes are empty,
+    #allow the user to exit the window without being prompted again.
     if house_price.get() == "" and equity.get() == "" and salary.get() == "" and monthly_expenses.get() == "":
         exit_workflow()
-
+    
+    #Check if input can be converted to integers
     try:
         int(house_price.get())
         int(equity.get())
         int(salary.get())
         int(monthly_expenses.get())
     except ValueError:
-        return False
+        return False #Return False if input cannot be converted to integers
 
     with open("userinput.txt", "w", encoding="utf-8") as f:
         f.write(f"{house_price.get()} {equity.get()} {salary.get()} {monthly_expenses.get()}")
-    return True
+    return True #Return True if input is accepted
 
+#Create dialogue window informing about exiting input windows with incomplete setup.
 def exit_workflow():
     #Setup of window
     root = tk.Tk()
@@ -96,7 +101,6 @@ def exit_workflow():
     root.mainloop()
     exit()
 
-input_is_valid = get_user_input("Hi! Please fill in your information to simulate when you will be debt free!\n\n")
-
-while not input_is_valid:  
-    input_is_valid = get_user_input("Sorry, you have entered something invalid, please enter only numbers")
+input_is_valid = get_user_input("Hi! Please fill in your information to simulate when you will be debt free!\n(Enter empty boxes to exit)\n\n")
+while not input_is_valid:  #Keep asking for input until input is valid
+    input_is_valid = get_user_input("Sorry, you have entered something invalid, please enter only numbers\n(Enter empty boxes to exit)\n\n")
